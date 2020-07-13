4c5628fbac6f3260d4050c36cc211994858c3ecd
richardliu1229 <67864523+richardliu1229@users.noreply.github.com>
2020-07-13 08:26:31 -0400

    Adding empty templates
    Starting template using the pygame library

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Step 1: First initalize the map by reading in csv
+
+
+
+
+
+"""
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((1000,1000))
+pygame.display.set_caption("Safest Path")
+
+while True:
+    pygame.time.delay(100)
+
+
+
---
+++ safestPathAlg.py
@@ -0,0 +1,16 @@
+""" We should work on these """
+
+
+# Find all possible routes to destination, shortest and safest
+
+
+
+
+
+# find distance by summing up, for all potential paths
+
+
+
+
+
+## find shortest path
