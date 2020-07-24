544e37c873728079eb4f501d5cfb22dc509e5376
benjaminhsu99 <52979228+benjaminhsu99@users.noreply.github.com>
2020-07-24 14:33:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import PyGameMain
+import pygameMain
 import safestPathAlg
 
 #MAIN FUNCTION
-from Mapinit import bigMapFileName
+import Mapinit
 
 if __name__ == '__main__':
     #COMMAND MENU
@@ -10,24 +10,30 @@ if __name__ == '__main__':
     while(userInput != "exit"):
         #enter algorithms-submenu
         if(userInput == "1"):
+            #specify that the CSV file being used is the small size one
+            Mapinit.mapFileName = Mapinit.smallMapFileName
             userInput = input("\nEnter \t\"1\" to perform visual GUI shortest-path search (small grid)\n\t\"2\" to perform text-based shortest path search (large grid)\n\t\"3\" to perform text-based multiple shortest paths search (large grid)\n\t\"cancel\" to return to the main menu\n(type in here): ")
             while(userInput != "1" and userInput != "2" and userInput != "3" and userInput != "cancel"):
                 userInput = input("\nEnter \t\"1\" to perform visual GUI shortest-path search (small grid)\n\t\"2\" to perform text-based shortest path search (large grid)\n\t\"3\" to perform text-based multiple shortest paths search (large grid)\n\t\"cancel\" to return to the main menu\n(type in here): ")
             #single shortest path visual GUI function
             if(userInput == "1"):
-                PyGameMain.main_menu()
+                pygameMain.main_menu()
             #single shortest path text function command
             elif(userInput == "2"):
                 #specify that the CSV file being used is the big size one
-                mapFileName = bigMapFileName
+                Mapinit.mapFileName = Mapinit.bigMapFileName
                 #first find the dimensions of the map
                 rows, columns = safestPathAlg.findDimensionsOfMap()
                 #load a new map & adjacency list instance
                 array = safestPathAlg.createArray(rows, columns)
                 adjacencyList, intersectionNameDictionary = safestPathAlg.createAdjacencyList(array, rows, columns)
                 #receive user-input
-                sourceIntersectionName = input("Enter name of starting street intersection (e.g. \"Baker / Wilson\"): ")
-                destinationIntersectionName = input("Enter name of destination street intersection (e.g. \"Chevrolet / Sushi\"): ")
+                if(Mapinit.useNumbersInsteadOfStreetNames == False):
+                    sourceIntersectionName = input("Enter name of starting street intersection (e.g. \"Chevrolet / Westbrooke\"): ")
+                    destinationIntersectionName = input("Enter name of destination street intersection (e.g. \"Studebaker / Rochelle\"): ")
+                else:
+                    sourceIntersectionName = input("Enter name of starting street intersection (e.g. \"0 / 0\"): ")
+                    destinationIntersectionName = input("Enter name of destination street intersection (e.g. \"1 / 1\"): ")
                 weightMode = input("Factor in crime map? (\"y\") or find the strictly shortest physical path (\"n\")?: ")
                 while(weightMode != "y" and weightMode != "n"):
                     weightMode = input("Factor in crime map? (\"y\") or find the strictly shortest physical path (\"n\")?: ")
@@ -40,12 +46,16 @@ if __name__ == '__main__':
             #multiple shortest path text function command
             elif(userInput == "3"):
                 #specify that the CSV file being used is the big size one
-                mapFileName = bigMapFileName
+                Mapinit.mapFileName = Mapinit.bigMapFileName
                 #first find the dimensions of the map
                 rows, columns = safestPathAlg.findDimensionsOfMap()
                 #receive user-input
-                sourceIntersectionName = input("Enter name of starting street intersection (e.g. \"Baker / Wilson\"): ")
-                destinationIntersectionName = input("Enter name of destination street intersection (e.g. \"Chevrolet / Sushi\"): ")
+                if(Mapinit.useNumbersInsteadOfStreetNames == False):
+                    sourceIntersectionName = input("Enter name of starting street intersection (e.g. \"Chevrolet / Westbrooke\"): ")
+                    destinationIntersectionName = input("Enter name of destination street intersection (e.g. \"Studebaker / Rochelle\"): ")
+                else:
+                    sourceIntersectionName = input("Enter name of starting street intersection (e.g. \"0 / 0\"): ")
+                    destinationIntersectionName = input("Enter name of destination street intersection (e.g. \"1 / 1\"): ")
                 weightMode = input("Factor in crime map? (\"y\") or find the strictly shortest physical path (\"n\")?: ")
                 while(weightMode != "y" and weightMode != "n"):
                     weightMode = input("Factor in crime map? (\"y\") or find the st1ictly shortest physical path (\"n\")?: ")
@@ -64,8 +74,8 @@ if __name__ == '__main__':
             while(userInput != "1" and userInput != "2" and userInput != "cancel"):
                 userInput = input("\nEnter,\n\t\"1\" to enable street-names (e.g. \"Chevrolet / Wilson\"),\n\t\"2\" to enable indicies as street names (e.g. \"0 / 0\"),\n\t\"cancel\" to cancel\n(type in here): ")
             if(userInput == "1"):
-                useNumbersInsteadOfStreetNames = False
+                Mapinit.useNumbersInsteadOfStreetNames = False
             elif(userInput == "2"):
-                useNumbersInsteadOfStreetNames = True
+                Mapinit.useNumbersInsteadOfStreetNames = True
         #loop for the main menu
         userInput = input("\nEnter \"1\" to begin a shortest-path search, enter \"2\" to configure options, enter \"exit\" to exit: ")
